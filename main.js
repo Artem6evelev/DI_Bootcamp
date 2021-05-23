cfa291eeaf94527f42e4f336b608cb41d0335b0b
Artem Shevelev <74210822+Artem6evelev@users.noreply.github.com>
2021-05-23 22:37:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const nodemon = require("nodemon");
+
+//* Exercise 1: Hello nodemon.JS
+//* Exercise 2 : Hello Node.Js #2
+let a = 5;
+let b = 10;
+console.log(`Hello you are ${a + b}`);
+console.log('Hi, thanks for running me with npm start!')
+
+//*  Exercise 3 : Nodemon
+let x;
+let c = 6;
+x = a + b;
+console.log(x + c);
